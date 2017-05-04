82549254991ea2694118f176c00c42d0c423e64e
1114902352 <1114902352@qq.com>
2017-05-04 10:55:40 +0800

    ssssss

--- kbtc.secruity/src/main/java/DelLastUpdated.java
+++ kbtc.secruity/src/main/java/DelLastUpdated.java
@@ -17,7 +17,7 @@ public class DelLastUpdated {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.print("aaaaaaaaaaaaa");
+		System.out.print("ccccc");
 		File mavenRep = new File(MAVEN_REPO_PATH);
 		if (!mavenRep.exists()) {
 			_log.warn("Maven repos is not exist.");
